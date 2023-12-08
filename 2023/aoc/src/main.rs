513faaaf1593b74ed3765cba9aca306447f03aba
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-08 11:10:51 -0500

    adds basic commands and file parsing

--- 2023/aoc/src/main.rs
+++ 2023/aoc/src/main.rs
@@ -1,22 +1,52 @@
 use clap::Parser;
+use std::{fs::File, io::Read, path::PathBuf};
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(
+    author,
+    version,
+    about,
+    long_about = "A small CLI app used to run AoC puzzles"
+)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+    #[arg(long_help = "The AoC puzzle day to run", required = true)]
+    day: u32,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[arg(
+        long_help = "The AoC puzzle day's part to run. Can be either 1 or 2.",
+        required = true
+    )]
+    part: u32,
+
+    #[arg(
+        long_help = "The file to use as input for the day+part",
+        required = true
+    )]
+    file: PathBuf,
 }
 
 fn main() {
     let args = Args::parse();
+    println!("Day: {}", args.day);
+    println!("Part: {}", args.part);
+    println!("Input file: {:?}", args.file.to_string_lossy());
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
-    }
+    let contents = read_file(args.file);
+    println!("File content:\n{}", contents);
+}
+
+fn read_file(file: PathBuf) -> String {
+    let path = file.as_path();
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    return match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => s,
+    };
 }
