665001754721c4823ed8efe2a95d43ef617e6b57
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-08 16:13:29 -0500

    solves 2023 day 1 part 1

--- aoc-2023-rust/day01-part01/src/main.rs
+++ aoc-2023-rust/day01-part01/src/main.rs
@@ -1,3 +1,26 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+
+    let mut s: u32 = 0;
+    let mut e: u32 = 0;
+    let mut n: u32 = 0;
+    for line in stdin.lock().lines() {
+        for ch in line.as_ref().unwrap().chars() {
+            if ch.is_digit(10) {
+                s = ch.to_digit(10).unwrap();
+                break;
+            }
+        }
+
+        for ch in line.unwrap().chars().rev() {
+            if ch.is_digit(10) {
+                e = ch.to_digit(10).unwrap();
+                break;
+            }
+        }
+        n = n + s * 10 + e;
+    }
+    println!("{}", n);
 }
