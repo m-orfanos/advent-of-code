ac255b6a8dd8074f345475e8ba1f288f8b824ca5
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-09 17:59:14 -0500

    solves AoC 2023 day 01 part 02
    Signed-off-by: Miltiadis Orfanos <morfanos@tuta.io>

--- aoc-2023-rust/day01-part02/src/main.rs
+++ aoc-2023-rust/day01-part02/src/main.rs
@@ -1,3 +1,131 @@
+use std::io::{self, BufRead};
+
+struct WordDigit {
+    word: &'static str,
+    digit: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let word_digits = [
+        WordDigit {
+            word: "zero",
+            digit: 0,
+        },
+        WordDigit {
+            word: "one",
+            digit: 1,
+        },
+        WordDigit {
+            word: "two",
+            digit: 2,
+        },
+        WordDigit {
+            word: "three",
+            digit: 3,
+        },
+        WordDigit {
+            word: "four",
+            digit: 4,
+        },
+        WordDigit {
+            word: "five",
+            digit: 5,
+        },
+        WordDigit {
+            word: "six",
+            digit: 6,
+        },
+        WordDigit {
+            word: "seven",
+            digit: 7,
+        },
+        WordDigit {
+            word: "eight",
+            digit: 8,
+        },
+        WordDigit {
+            word: "nine",
+            digit: 9,
+        },
+    ];
+
+    let stdin = io::stdin();
+    let mut n = 0;
+    for line in stdin.lock().lines() {
+        let haystack = line.as_ref().unwrap();
+
+        let first = find_forward(&word_digits, haystack);
+        let last = find_backward(&word_digits, haystack);
+
+        n += first * 10 + last;
+    }
+    println!("{}", n);
+}
+
+fn find_forward(word_digits: &[WordDigit], haystack: &String) -> i32 {
+    let mut idx = usize::MAX;
+    let mut n = -1;
+
+    // search for digits as words (zero, one, two, ..., nine)
+    for word_digit in word_digits {
+        let needle = word_digit.word;
+        match haystack.find(needle) {
+            Some(match_idx) => {
+                if match_idx < idx {
+                    // update only when a better match is found
+                    idx = match_idx;
+                    n = word_digit.digit;
+                }
+            }
+            None => (),
+        }
+    }
+
+    // iterate over haystack searching for digits (0, 1, 2, ..., 9)
+    for (current_idx, current_ch) in haystack.char_indices() {
+        if current_idx > idx {
+            // already found a better match
+            break;
+        }
+        if current_ch.is_digit(10) {
+            idx = current_idx;
+            n = current_ch.to_digit(10).unwrap() as i32;
+        }
+    }
+
+    n
+}
+
+fn find_backward(word_digits: &[WordDigit], haystack: &String) -> i32 {
+    let mut idx = usize::MIN;
+    let mut n = -1;
+
+    // search for digits as words (zero, one, two, ..., nine) wrt the end
+    for word_digit in word_digits {
+        let needle = word_digit.word;
+        match haystack.rfind(needle) {
+            Some(match_idx) => {
+                if match_idx > idx {
+                    // update only when a better match is found
+                    idx = match_idx;
+                    n = word_digit.digit;
+                }
+            }
+            None => (),
+        }
+    }
+
+    // iterate over haystack in reverse searching for digits (0, 1, 2, ..., 9)
+    for (current_idx, current_ch) in haystack.char_indices().rev() {
+        if current_idx < idx {
+            // already found a better match
+            break;
+        }
+        if current_ch.is_digit(10) {
+            idx = current_idx;
+            n = current_ch.to_digit(10).unwrap() as i32;
+        }
+    }
+
+    n
 }
---
+++ docs/README.md
@@ -0,0 +1,9 @@
+# Advent of Code notebook
+
+This directory contains notes I took while solving puzzles.
+
+- The `journal` directory contains my thoughts/approaches over the course of the challenges, in the form of "daily notes".
+- The `puzzles` directory contains a file for each puzzle; they are mostly empty.
+- The `templates` directory contains templates; they are used as the base when creating new notes.
+
+The docs are stuctured this way because I take notes using [obsidian](https://obsidian.md/), which helps me streamline everything.
