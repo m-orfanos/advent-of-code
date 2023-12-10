4881c324c14d4e4e45b1496ccbf67ffa62a2312f
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-10 18:00:11 -0500

    reimplements day 03

--- aoc-2023-rust/day03-part02/src/main.rs
+++ aoc-2023-rust/day03-part02/src/main.rs
@@ -1,14 +1,12 @@
 use std::io::{self, BufRead};
 
 fn main() {
-    #[derive(Debug)]
     struct EnginePart {
         number: u32,
         row: i32,
         cols: Vec<usize>,
     }
 
-    #[derive(Debug)]
     struct Symbol {
         // ch: char, // might be useful in part 2, it was not
         row: i32,
@@ -25,51 +23,46 @@ fn main() {
     // parse all part numbers and symbols
     // while keeping track of their coordinates
     for line_res in io::stdin().lock().lines() {
+        let line = line_res.unwrap();
         row += 1;
 
         let mut number = 0;
+        let mut is_parsing_digits = false;
+        let mut cols: Vec<usize> = Vec::new();
 
-        match line_res {
-            Ok(line) => {
-                let mut is_parsing_digits = false;
-                let mut cols: Vec<usize> = Vec::new();
-
-                for (col, ch) in line.char_indices() {
-                    if ch.is_digit(10) {
-                        // build number char by char
-                        is_parsing_digits = true;
-                        number = number * 10 + ch.to_digit(10).unwrap();
-                        cols.push(col);
-                    } else {
-                        // matched a symbol
-                        if is_parsing_digits {
-                            // was building a number
-                            parts.push(EnginePart {
-                                number,
-                                row,
-                                cols: cols.clone(),
-                            });
-                            number = 0;
-                            cols = Vec::new();
-                        }
-                        is_parsing_digits = false;
-                        match ch {
-                            '*' => symbols.push(Symbol { row, col }),
-                            _ => (),
-                        }
-                    }
-                }
-
-                // part number is at the end of the input
+        for (col, ch) in line.char_indices() {
+            if ch.is_digit(10) {
+                // build number char by char
+                is_parsing_digits = true;
+                number = number * 10 + ch.to_digit(10).unwrap();
+                cols.push(col);
+            } else {
+                // matched a symbol
                 if is_parsing_digits {
+                    // was building a number
                     parts.push(EnginePart {
                         number,
                         row,
                         cols: cols.clone(),
                     });
+                    number = 0;
+                    cols = Vec::new();
+                }
+                is_parsing_digits = false;
+                match ch {
+                    '*' => symbols.push(Symbol { row, col }),
+                    _ => (),
                 }
             }
-            Err(e) => panic!("{:?}", e),
+        }
+
+        // part number is at the end of the input
+        if is_parsing_digits {
+            parts.push(EnginePart {
+                number,
+                row,
+                cols: cols.clone(),
+            });
         }
     }
 
