10633ab5493596cdb33a687c078c5cbb1d7953f6
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-10 11:21:10 -0500

    solves AoC 2023 day 03 part 01
    Signed-off-by: Miltiadis Orfanos <morfanos@tuta.io>

---
+++ aoc-2023-rust/day03-part01/src/main.rs
@@ -0,0 +1,111 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    #[derive(Debug)]
+    struct EnginePart {
+        number: u32,
+        row: i32,
+        cols: Vec<usize>,
+    }
+
+    #[derive(Debug)]
+    struct Symbol {
+        // ch: char, // might be useful in part 2
+        row: i32,
+        col: usize,
+    }
+
+    let mut parts: Vec<EnginePart> = Vec::new();
+    let mut symbols: Vec<Symbol> = Vec::new();
+
+    // keep track of the line number
+    // visualize input as a matrix with <r> rows and <c> columns
+    let mut row = -1;
+
+    // parse all part numbers and symbols
+    // while keeping track of their coordinates
+    for line_res in io::stdin().lock().lines() {
+        row += 1;
+
+        let mut number = 0;
+
+        match line_res {
+            Ok(line) => {
+                let mut is_parsing_digits = false;
+                let mut cols: Vec<usize> = Vec::new();
+
+                for (col, ch) in line.char_indices() {
+                    if ch.is_digit(10) {
+                        // build number char by char
+                        is_parsing_digits = true;
+                        number = number * 10 + ch.to_digit(10).unwrap();
+                        cols.push(col);
+                    } else {
+                        // matched a symbol
+                        if is_parsing_digits {
+                            // was building a number
+                            parts.push(EnginePart {
+                                number,
+                                row,
+                                cols: cols.clone(),
+                            });
+                            number = 0;
+                            cols = Vec::new();
+                        }
+                        is_parsing_digits = false;
+                        match ch {
+                            '.' => (),
+                            _ => symbols.push(Symbol { row, col }),
+                        }
+                    }
+                }
+
+                // part number is at the end of the input
+                if is_parsing_digits {
+                    parts.push(EnginePart {
+                        number,
+                        row,
+                        cols: cols.clone(),
+                    });
+                }
+            }
+            Err(e) => panic!("{:?}", e),
+        }
+    }
+
+    // calculate sum of part numbers near symbols
+    let mut ans = 0;
+
+    for EnginePart {
+        number: n,
+        row: erow,
+        cols: ecols,
+    } in parts
+    {
+        for Symbol {
+            // ch: _,
+            row: srow,
+            col: scol,
+        } in &symbols
+        {
+            if *srow < erow - 1 {
+                // symbol is at least 2 rows above part
+                continue;
+            }
+            if *srow > erow + 1 {
+                // symbol is at least 2 rows below part
+                // no remaining symbols near part
+                break;
+            }
+            for ecol in &ecols {
+                if scol - 1 <= *ecol && *ecol <= scol + 1 {
+                    // symbol is adjacent to part
+                    ans += n;
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
+}
