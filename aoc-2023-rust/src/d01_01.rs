27a95cca0356c12f461383df2da143d2ff84e889
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-14 15:15:55 -0500

    uses clap to route to correct file

---
+++ aoc-2023-rust/src/d01_01.rs
@@ -0,0 +1,36 @@
+use std::io::{self, BufRead};
+
+pub fn solve() {
+    let mut ans: u32 = 0;
+    for line_res in io::stdin().lock().lines() {
+        let line = line_res.unwrap();
+
+        let s = find_digit(&line);
+        let e = rfind_digit(&line);
+
+        // build the number
+        // s/e will always be single digit numbers
+        ans += s * 10 + e
+    }
+
+    println!("{}", ans);
+}
+
+fn find_digit(line: &str) -> u32 {
+    // using unwrap because there will ALWAYS be a value present
+    line.chars()
+        .find(|ch| ch.is_digit(10))
+        .map(|ch| ch.to_digit(10))
+        .unwrap()
+        .unwrap()
+}
+
+fn rfind_digit(line: &str) -> u32 {
+    // using unwrap because there will ALWAYS be a value present
+    line.chars()
+        .rev()
+        .find(|ch| ch.is_digit(10))
+        .map(|ch| ch.to_digit(10))
+        .unwrap()
+        .unwrap()
+}
