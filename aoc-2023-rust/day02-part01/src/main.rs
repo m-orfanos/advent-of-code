e4c82dd2cb917030f6f6905432efb9d72f7ceff6
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-14 02:08:28 -0500

    fix build errors

--- aoc-2023-rust/day02-part01/src/main.rs
+++ aoc-2023-rust/day02-part01/src/main.rs
@@ -1,4 +1,3 @@
-use crates::parsers::split;
 use std::{
     collections::HashMap,
     io::{self, BufRead},
@@ -13,26 +12,25 @@ fn main() {
 
         // parse line, format
         // Game x: n1 colour1, n2 colour2; m1 colour2, m2 colour3; ...
-        let game_rounds = split(&line, ":");
+        let game_rounds: Vec<&str> = line.split(":").map(|x| x.trim()).collect();
 
         // parse Game, format
         // Game x
-        let game = split(game_rounds[0], " ");
+        let game: Vec<&str> = game_rounds[0].split(" ").map(|x| x.trim()).collect();
         let game_id = i32::from_str_radix(game[1], 10).unwrap();
 
         // parse rounds, format
         // n1 colour1, n2 colour2; m1 colour2, m2 colour3; ...
         let mut is_possible = true;
-        for round_str in split(game_rounds[1], ";") {
+        for round_str in game_rounds[1].split(";").map(|x| x.trim()) {
             // parse round, format
             // n1 colour1, n2 colour2, ...
-            for set_str in split(round_str, ",") {
+            for set_str in round_str.split(",").map(|x| x.trim()) {
                 // parse set, format
                 // n1 colour1
-                let set = split(set_str, " ");
-                let n = i32::from_str_radix(set[0], 10).unwrap();
-                let colour = set[1];
-                if n > colours_map[colour] {
+                let set: Vec<&str> = set_str.split(" ").map(|x| x.trim()).collect();
+                let n = i32::from_str_radix(&set[0], 10).unwrap();
+                if n > colours_map[&set[1]] {
                     is_possible = false;
                     break;
                 }
