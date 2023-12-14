b83a5fe064c02584e8dca1eacfe03c2509f25b2d
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-14 02:08:28 -0500

    fix build errors

--- aoc-2023-rust/day02-part02/src/main.rs
+++ aoc-2023-rust/day02-part02/src/main.rs
@@ -1,4 +1,3 @@
-use crates::parsers::split;
 use std::{
     collections::HashMap,
     io::{self, BufRead},
@@ -15,17 +14,17 @@ fn main() {
 
         // parse line, format
         // Game x: n1 colour1, n2 colour2; m1 colour2, m2 colour3; ...
-        let game_rounds = split(&input, ":");
+        let game_rounds: Vec<&str> = input.split(":").map(|x| x.trim()).collect();
 
         // parse rounds, format
-        // n1 colour1, n2 colour2; m1 colour2, m2 colour3; ...
-        for round_str in split(&game_rounds[1], ";") {
+        // n1 colour1, n2 colour2; m1 colour2, m2 colour3; ..
+        for round_str in game_rounds[1].split(";").map(|x| x.trim()) {
             // parse round, format
             // n1 colour1, n2 colour2, ...
-            for set_str in split(&round_str, ",") {
+            for set_str in round_str.split(",").map(|x| x.trim()) {
                 // parse set, format
                 // n1 colour1
-                let set = split(set_str, " ");
+                let set: Vec<&str> = set_str.split(" ").map(|x| x.trim()).collect();
                 let n = i32::from_str_radix(set[0], 10).unwrap();
                 let colour = set[1];
 
