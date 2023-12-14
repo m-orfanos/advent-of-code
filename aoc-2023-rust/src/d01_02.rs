e295d933c30e5520f7130ca607be44db13987ea6
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-14 15:39:44 -0500

    swaps to clap derive

---
+++ aoc-2023-rust/src/d01_02.rs
@@ -0,0 +1,105 @@
+use std::io::{self, BufRead};
+
+struct WordDigit<'a> {
+    word: &'a str,
+    digit: u32,
+}
+
+static WORD_DIGITS: &'static [WordDigit] = &[
+    WordDigit {
+        word: "zero",
+        digit: 0,
+    },
+    WordDigit {
+        word: "one",
+        digit: 1,
+    },
+    WordDigit {
+        word: "two",
+        digit: 2,
+    },
+    WordDigit {
+        word: "three",
+        digit: 3,
+    },
+    WordDigit {
+        word: "four",
+        digit: 4,
+    },
+    WordDigit {
+        word: "five",
+        digit: 5,
+    },
+    WordDigit {
+        word: "six",
+        digit: 6,
+    },
+    WordDigit {
+        word: "seven",
+        digit: 7,
+    },
+    WordDigit {
+        word: "eight",
+        digit: 8,
+    },
+    WordDigit {
+        word: "nine",
+        digit: 9,
+    },
+];
+
+struct Calibration {
+    idx: usize,
+    digit: u32,
+}
+
+pub fn solve() {
+    let mut ans = 0;
+
+    for line in io::stdin().lock().lines() {
+        let haystack = line.unwrap();
+
+        // parse the words
+        let ws: Vec<Calibration> = WORD_DIGITS
+            .iter()
+            .flat_map(|wd| {
+                // return first and last, if applicable
+                vec![
+                    haystack.find(wd.word).map(|idx| Calibration {
+                        idx,
+                        digit: wd.digit,
+                    }),
+                    haystack.rfind(wd.word).map(|idx| Calibration {
+                        idx,
+                        digit: wd.digit,
+                    }),
+                ]
+            })
+            .filter(|w| w.is_some())
+            .map(|w| w.unwrap())
+            .collect();
+
+        // parse the digits
+        let ds: Vec<Calibration> = haystack
+            .char_indices()
+            .filter(|tup| tup.1.is_digit(10))
+            .map(|tup| Calibration {
+                idx: tup.0,
+                digit: tup.1.to_digit(10).unwrap(),
+            })
+            .collect();
+
+        // merge words and digits in a single vector
+        let mut calibrations: Vec<&Calibration> = vec![&ws, &ds].into_iter().flatten().collect();
+        calibrations.sort_by(|a, b| a.idx.cmp(&b.idx));
+
+        // find first and last digits
+        let n = calibrations.first().unwrap();
+        let m = calibrations.last().unwrap();
+
+        // build the number
+        ans += n.digit * 10 + m.digit;
+    }
+
+    println!("{}", ans);
+}
--- aoc-2023-rust/src/main.rs
+++ aoc-2023-rust/src/main.rs
@@ -1,22 +1,28 @@
 mod d01_01;
+mod d01_02;
 
-use std::{collections::HashMap, path::PathBuf};
+use clap::{arg, command, Parser};
 
-use clap::{arg, command, value_parser, ArgAction, Command};
-use std::io::{self, BufRead};
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// AoC day
+    #[arg()]
+    day: i32,
+
+    /// AoC day part
+    #[arg()]
+    part: i32,
+}
 
 fn main() {
-    let mut puzzles: HashMap<i32, HashMap<i32, fn()>> = HashMap::new();
-    puzzles.insert(1, HashMap::new());
-    puzzles.get_mut(&1).and_then(|m| m.insert(1, d01_01::solve));
-
-    let matches = command!() 
-        .arg(arg!([day] "Day"))
-        .arg(arg!([part] "Part"))
-        .get_matches();
-
-    let day = matches.get_one::<String>("day").unwrap().parse::<i32>().unwrap();
-    let part = matches.get_one::<String>("part").unwrap().parse::<i32>().unwrap();
-
-    puzzles.get(&day).and_then(|m| m.get(&part)).and_then(|f| Some(f()));
+    let cli =  Cli::parse();
+    match cli.day {
+        1 => match cli.part {
+            1 => d01_01::solve(),
+            2 => d01_02::solve(),
+            _ => todo!(),
+        },
+        _ => todo!(),
+    }
 }
