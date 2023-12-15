8a1fe44cbf9121f7bb55471c3f57400a8241fac7
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-15 15:56:35 -0500

    adds unit tests

--- aoc-2023-rust/src/main.rs
+++ aoc-2023-rust/src/main.rs
@@ -1,6 +1,6 @@
 // use std::{fs::File, io::BufReader};
 
-use clap::{Parser};
+use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -15,7 +15,7 @@ struct Cli {
 }
 
 fn main() {
-    let cli =  Cli::parse();
+    let cli = Cli::parse();
 
     // let filepath = "resources/day01-input-test";
     // let file = File::open(filepath);
@@ -28,5 +28,59 @@ fn main() {
             _ => todo!(),
         },
         _ => todo!(),
+    }
+}
+
+#[cfg(test)]
+mod aoc {
+    use assert_cmd::Command;
+    use std::fs::{read_to_string, File};
+    use std::io::{BufRead, BufReader};
+
+    #[test]
+    fn tests() {
+        let solutions_reader = BufReader::new(File::open("resources/solutions").unwrap());
+        let solutions_str: Vec<_> = solutions_reader.lines().map(|l| l.unwrap()).collect();
+
+        let days = ["01", "02", "03", "04", "05", "06"];
+        let parts = ["01", "02"];
+        for day in days {
+            for part in parts {
+                println!("Day {} Part {}...", day, part);
+
+                let cmd = format!("{day}_{part}");
+                let mut cmd = Command::cargo_bin(cmd).unwrap();
+
+                // test input
+                let path1 = format!("resources/day{day}-input-test");
+                let data1 = read_to_string(path1).unwrap();
+                let k1 = format!("{day}_{part}_01");
+                let solution_str1: Vec<_> = solutions_str
+                    .iter()
+                    .find(|b| b.starts_with(&k1))
+                    .unwrap()
+                    .split("=")
+                    .collect();
+                let p1 = solution_str1[1].to_string();
+
+                let assert1 = cmd.write_stdin(data1).assert();
+                assert1.success().stdout(format!("{p1}\n"));
+
+                // puzzle input
+                let path2 = format!("resources/day{day}-input");
+                let data2 = read_to_string(path2).unwrap();
+                let k2 = format!("{day}_{part}_02");
+                let solution_str2: Vec<_> = solutions_str
+                    .iter()
+                    .find(|b| b.starts_with(&k2))
+                    .unwrap()
+                    .split("=")
+                    .collect();
+                let p2 = solution_str2[1].to_string();
+
+                let assert2 = cmd.write_stdin(data2).assert();
+                assert2.success().stdout(format!("{p2}\n"));
+            }
+        }
     }
 }
