267ec0c1d3c40b28c905b389b84bda2813993e2d
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-16 03:26:26 -0500

    solves it

--- aoc-2023-rust/src/bin/08_02.rs
+++ aoc-2023-rust/src/bin/08_02.rs
@@ -3,11 +3,12 @@ use std::{
     io::{self, BufRead},
 };
 
+use aoc::common::lcm;
+
 fn main() {
+    // parse input
     let mut it = io::stdin().lock().lines();
-
     let instructions = it.next().unwrap().unwrap();
-
     it.next(); // blank line
 
     let mut map: HashMap<String, [String; 2]> = HashMap::new();
@@ -22,40 +23,142 @@ fn main() {
         map.insert(key, [left, right]);
     }
 
+    // find starting nodes
     let mut curr_nodes = Vec::new();
     for k in map.keys() {
         if k.ends_with("A") {
             curr_nodes.push(k);
         }
     }
+    curr_nodes.sort();
 
-    let mut instruction_counter = 0;
-    let mut cnt = 0;
-    let mut ends_with_z = false;
-    while !ends_with_z {
-        ends_with_z = true;
-        if instruction_counter >= instructions.len() {
-            instruction_counter = 0;
-        }
+    // traverse each starting node until a match is found (e.g. ends with z)
+    // starting from that point, calculate the number of steps until the matched
+    // node is reached again (every path is a closed loop; e.g. eventually repeats itself)
+    let mut path_lengths = vec![];
+    for j in 0..curr_nodes.len() {
+        let mut cnt = 0;
+        let mut cnt2 = 0;
+        let mut instruction_counter = 0;
+        let mut ends_with_z = false;
+        while !ends_with_z {
+            cnt += 1;
 
-        let instruction = instructions.chars().nth(instruction_counter).unwrap();
-        let index = if instruction == 'L' { 0 } else { 1 };
-
-        let mut next_nodes = Vec::new();
-        for curr in curr_nodes.iter() {
-            let nodes: &[String; 2] = &map[*curr];
-            let next = &nodes[index];
-            if !next.ends_with("Z") {
-                ends_with_z = false;
+            ends_with_z = false;
+            if instruction_counter >= instructions.len() {
+                instruction_counter = 0;
             }
-            next_nodes.push(next);
+
+            let instruction = instructions.chars().nth(instruction_counter).unwrap();
+            let index = if instruction == 'L' { 0 } else { 1 };
+
+            let mut next_nodes = Vec::new();
+            for (k, curr) in curr_nodes.iter().enumerate() {
+                let nodes: &[String; 2] = &map[*curr];
+                let next = &nodes[index];
+                if next.ends_with("Z") && j == k {
+                    if cnt2 <= 0 {
+                        cnt = 0;
+                        cnt2 += 1;
+                    } else {
+                        ends_with_z = true;
+                    }
+                }
+                next_nodes.push(next);
+            }
+
+            curr_nodes = next_nodes;
+            instruction_counter += 1;
         }
-
-        println!("{} {} {} {:?}->{:?}", cnt, instruction_counter, instruction, curr_nodes, next_nodes);
-
-        curr_nodes = next_nodes;
-        cnt += 1;
-        instruction_counter += 1;
+        path_lengths.push(cnt);
     }
-    println!("{}", cnt);
+
+    // here are the results for the computation above
+    // ["AAA", "DPA", "GTA", "QLA", "VJA", "XQA"]
+    // 0->12361
+    // 1->20777
+    // 2->16043
+    // 3->19199
+    // 4->18673
+    // 5->15517
+    //
+    // the first path repeats itself after 12,361 steps and the second after 20,777
+    //
+    // when will these two paths intersect?
+    // for the first path it takes 79 cycles and the second path its 47 cycles
+    // 79*12,361 == 47*20,777 = 976,519
+    //
+    // (Note how the numbers 47 and 79 are special... they are all primes. This is a clue.)
+    //
+    // using a smaller example
+    // 0->2
+    // 1->3
+    //
+    //  steps 123456
+    // path 0 ->->->|
+    // path 1 -->-->|
+    // where `-` denotes a step and `>` a match/destination
+    //
+    // using a slightly larger example
+    // 0->14 (2*7)
+    // 1->16 (2*2*2*2)
+    // calculate the prime factors, take the common factors and multiply them together
+    // 2*2*2*2*7=112
+    //                10         20        30        40        50        60        70        80        90        100       110
+    //        ---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|
+    //        0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678912
+    // path 0 ------------->------------->------------->------------->------------->------------->------------->------------->|
+    // path 1 --------------->--------------->--------------->--------------->--------------->--------------->--------------->|
+    // (it's 112 steps)
+    //
+    // we've essentially calculated the lowest-common-multiple of two numbers
+    // now we need to do it for all paths
+    let ans = lcm(path_lengths);
+    println!("{}", ans);
+
+    // code used to explore the input
+    // prints out the lengths of the paths for 2 nodes
+    // for i in 0..curr_nodes.len() {
+    //     for j in i + 1..curr_nodes.len() {
+    //         let mut cnt = 0;
+    //         let mut cnt2 = 0;
+    //         let mut instruction_counter = 0;
+    //         let mut is_match = false;
+    //         let mut ends_with_z = false;
+    //         while !ends_with_z {
+    //             cnt += 1;
+    //             ends_with_z = false;
+    //             if instruction_counter >= instructions.len() {
+    //                 instruction_counter = 0;
+    //             }
+
+    //             let instruction = instructions.chars().nth(instruction_counter).unwrap();
+    //             let index = if instruction == 'L' { 0 } else { 1 };
+
+    //             let mut next_nodes = Vec::new();
+    //             for (k, curr) in curr_nodes.iter().enumerate() {
+    //                 let nodes: &[String; 2] = &map[*curr];
+    //                 let next = &nodes[index];
+    //                 if next.ends_with("Z") && (i == k || j == k) {
+    //                     cnt2 += 1;
+    //                 }
+    //                 next_nodes.push(next);
+    //             }
+
+    //             if cnt2 == 2 {
+    //                 if !is_match {
+    //                     is_match = true;
+    //                     cnt = 0;
+    //                 } else {
+    //                     ends_with_z = true;
+    //                 }
+    //             }
+
+    //             cnt2 = 0;
+    //             curr_nodes = next_nodes;
+    //             instruction_counter += 1;
+    //         }
+    //         println!("{},{},{}", i, j, cnt);
+    //     }
+    // }
 }
