4eb15c0d5115a0c87a03c1267a1892643a6f0831
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-16 13:21:20 -0500

    solves AoC 2023 day 09 part 01

---
+++ aoc-2023-rust/src/bin/09_01.rs
@@ -0,0 +1,52 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let mut prediction = 0;
+
+    for line_res in io::stdin().lock().lines() {
+        let line = line_res.unwrap();
+
+        // parse input
+        let history: Vec<i64> = line.split(" ").map(|n| n.trim().parse().unwrap()).collect();
+
+        // evaluate intermediary states
+        let mut pyramid: Vec<Vec<i64>> = vec![];
+        pyramid.push(history);
+        loop {
+            let next = get_next(&pyramid[pyramid.len() - 1]);
+            let cnt = next.iter().filter(|n| n.eq(&&0)).count();
+            if cnt == next.len() {
+                break;
+            }
+            pyramid.push(next);
+        }
+
+        // make prediction
+        let mut history_prediction = 0;
+        for row in pyramid {
+            // println!("{:?}", row);
+            history_prediction += row[row.len() - 1];
+        }
+
+        // println!("{}\n", history_prediction);
+
+        prediction += history_prediction;
+    }
+
+    println!("{}", prediction);
+}
+
+fn get_next(digits: &Vec<i64>) -> Vec<i64> {
+    let mut next = vec![];
+    let mut j = 1;
+    for i in 0..digits.len() - 1 {
+        let a = digits[i];
+        let b = digits[j];
+
+        let c = b - a;
+        next.push(c);
+
+        j += 1;
+    }
+    next
+}
