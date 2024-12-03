143e8d173a3caef9f025e23b468488293b6b108c
Miltiadis Orfanos <morfanos@tuta.io>
2024-12-03 14:39:36 -0500

    solves 2024 D02 P02

---
+++ 2024-ts/src/main/02_02.ts
@@ -0,0 +1,27 @@
+import { isSafeReport } from "./02_01.ts";
+import { convertTo2DArray } from "./utils.ts";
+
+export function solve(input: string): number {
+  let cnt = 0;
+  const grid: number[][] = convertTo2DArray(input);
+  for (let i = 0; i < grid.length; i++) {
+    const safe = isSafeReport(grid[i]);
+    if (safe) {
+      cnt += 1;
+      continue;
+    }
+
+    // TODO: optimize search space
+    for (let j = 0; j < grid[i].length; j++) {
+      const cpy = [...grid[i]];
+      cpy.splice(j, 1);
+      const safe = isSafeReport(cpy);
+      if (safe) {
+        cnt += 1;
+        break;
+      }
+    }
+  }
+
+  return cnt;
+}
