52f3a3ed15a758e436a5c98649aff4b36329f0b9
Miltiadis Orfanos <morfanos@tuta.io>
2024-12-04 20:28:12 -0500

    solves 2023 D04 P02

---
+++ aoc-ts/src/main/2023/04_02.ts
@@ -0,0 +1,21 @@
+import { toArrayNumeric } from "../utils/parsers.ts";
+
+export function solve(input: string): number {
+  const rows = input.trim().split("\n");
+  const cache: number[] = Array<number>(rows.length).fill(1);
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    const card = row.trim().split(":");
+    const numbers = card[1].trim().split("|");
+
+    const winners = new Set(toArrayNumeric(numbers[0]));
+    const player = new Set(toArrayNumeric(numbers[1]));
+
+    const overlap = winners.intersection(player);
+    for (let j = 0; j < overlap.size; j++) {
+      cache[i + j + 1] += cache[i];
+    }
+  }
+
+  return cache.reduce((acc, curr) => acc + curr, 0);
+}
