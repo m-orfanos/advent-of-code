2d8b3cbf879468a4369d2c5ff98602f844c048a0
Miltiadis Orfanos <morfanos@tuta.io>
2024-12-05 18:56:26 -0500

    solves 2024 D05 P01

---
+++ aoc-ts/src/main/2024/05_01.ts
@@ -0,0 +1,47 @@
+export function solve(input: string): number {
+  const rows = input.trim().split("\n");
+
+  // parse orders
+  const orders = new Map<number, number[]>();
+  let i = 0;
+  while (rows[i].trim().length !== 0) {
+    const [a, b] = rows[i].trim().split("|").map((s) => Number.parseInt(s, 10));
+    if (!orders.has(a)) {
+      orders.set(a, []);
+    }
+    orders.get(a)?.push(b);
+    i += 1;
+  }
+
+  let sum = 0;
+
+  // parse updates
+  i += 1;
+  while (i < rows.length && rows[i].trim().length !== 0) {
+    const deps = new Set<number>();
+    const update = rows[i].trim().split(",").map((s) => Number.parseInt(s, 10));
+    let isOrdered = true;
+    // starting from the end
+    // keep track of all dependencies
+    // a update has the right order if
+    // older values aren't part of the list
+    for (const x of update.reverse()) {
+      if (deps.has(x)) {
+        isOrdered = false;
+        break;
+      }
+      for (const y of orders.get(x) || []) {
+        deps.add(y);
+      }
+    }
+
+    if (isOrdered) {
+      // take sum
+      sum += update[Math.floor((update.length - 1) / 2)];
+    }
+
+    i += 1;
+  }
+
+  return sum;
+}
---
+++ aoc-ts/src/tests/2024/05_01.test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "jsr:@std/assert";
+import { solve } from "../solve.ts";
+
+Deno.test(`2024 D05 P01`, async () => {
+  const ans = await solve("2024", "05", "01");
+  assertEquals(ans, 4957);
+});
