c41779d8dff25a720b10e4197f8ebbdddf0f3d06
Miltiadis Orfanos <morfanos@tuta.io>
2024-12-17 23:56:29 -0500

    solves 2024 D09 P01-02

---
+++ aoc-ts/src/tests/2024/09_02.test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "jsr:@std/assert";
+import { solve } from "../solve.ts";
+
+Deno.test(`2024 D09 P02`, async () => {
+  const ans = await solve("2024", "09", "02");
+  assertEquals(ans, 6307279963620);
+});
