79786d20b31ac513d605f5215bf2830fbd8aeead
Miltiadis Orfanos <morfanos@tuta.io>
2024-12-24 14:18:55 -0500

    solves 2024 D19

---
+++ aoc-ts/src/main/2024/19_02.ts
@@ -0,0 +1,33 @@
+import { new1DArray } from "../utils/arrays.ts";
+import { to1DArrayString } from "../utils/parsers.ts";
+
+export function solve(input: string): number {
+  // parse
+  const tmp = to1DArrayString(input);
+  const patterns = tmp[0].split(",").map((s) => s.trim());
+  const designs = tmp.splice(2).map((s) => s.trim());
+
+  // bottom-up dynamic programming approach
+  // "rebuild" the design starting from the end
+  let cnt = 0;
+  for (const design of designs) {
+    const mem = new1DArray(design.length, () => 0);
+
+    const fpatterns = patterns.filter((p) => design.includes(p));
+    for (let i = design.length - 1; i >= 0; i--) {
+      for (const pattern of fpatterns) {
+        if (design.slice(i).indexOf(pattern) == 0) {
+          if (i + pattern.length == design.length) {
+            mem[i] += 1;
+          } else {
+            mem[i] += mem[i + pattern.length];
+          }
+        }
+      }
+    }
+
+    cnt += mem[0];
+  }
+
+  return cnt;
+}
